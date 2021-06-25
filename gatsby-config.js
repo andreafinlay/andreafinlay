653af08ecaae9a26db1aaaf4db214e8ce29e2818
Andrea <andrea.a.finlay@gmail.com>
2021-06-25 10:41:28 -0400

    udpdate readme and meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,55 +1,41 @@
-const path = require('path')
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
+const path = require("path")
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
   siteMetadata: {
-    title: 'franke.tech',
-    description: 'The personal website of Benedikt Franke',
-    author: 'Benedikt Franke'
+    title: "andreafinlay.com",
+    description: "Personal portfolio site for Andrea Finlay",
+    author: "Andrea Finlay",
   },
   plugins: [
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'images',
-        path: path.join(__dirname, '/src/images')
-      }
+        name: "images",
+        path: path.join(__dirname, "/src/images"),
+      },
     },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-source-graphql',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        typeName: 'GitHub',
-        fieldName: 'github',
-        url: 'https://api.github.com/graphql',
-        headers: {
-          // Learn about environment variables: https://gatsby.dev/env-vars
-          Authorization: `bearer ${process.env.GITHUB_TOKEN}`
-        },
-        // Additional options to pass to node-fetch
-        fetchOptions: {}
-      }
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+      },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
-      }
-    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ]
+  ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
-import { Repositories } from '../components/repositories'
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
 
 const IndexPage = () => {
   return (
@@ -14,11 +13,10 @@ const IndexPage = () => {
         description="This is the personal website of Benedikt Franke"
       />
       <h1>Hi people</h1>
-      <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <div style={{ maxWidth: "300px", marginBottom: "1.45rem" }}>
         <Image />
       </div>
       <h2>Open Source</h2>
-      <Repositories />
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   )
