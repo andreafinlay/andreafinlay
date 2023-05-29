0ffb08a58884b864a8942dfb340fefa164e98ffa
andreafinlay <andrea.a.finlay@gmail.com>
2023-05-29 15:04:54 -0400

    feat(revamp): update content cmp, implement hashnav, update scroll logic, update cmps barrel exports

--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -1,64 +1,131 @@
-import React, { MutableRefObject, useRef } from 'react';
-
+import React, { FC, useEffect, useRef, WheelEvent, createRef } from 'react';
 import { useSlidesContext } from '../../contexts';
 import { useAllMdx, useMountEffect } from '../../hooks';
 import { scrollHorizontally, scrollToElement } from '../../helpers';
-import { ArrowRight } from '../../assets/icons';
+import { NavArrow } from '../navArrow';
 import { Slide } from '../slide';
 import { Styled } from './content.styled';
 
-export const Content: React.FC = () => {
+export const Content: FC = () => {
     const edges = useAllMdx();
-    const { slideRefs, setSlideRefs, shouldShowArrow, setShouldShowArrow } =
-        useSlidesContext();
 
     const contentRef = useRef<HTMLDivElement>();
-    const slideRefsArr: Array<MutableRefObject<HTMLDivElement> | undefined> =
-        [];
 
-    const handleHorizontalScroll = (event) => {
-        const { scrollPosition } = scrollHorizontally(event, contentRef);
+    const {
+        slideRefs,
+        nextSlide,
+        setNextSlide,
+        findNextSlide,
+        shouldShowArrow,
+        isArrowVisible,
+        showArrow,
+        hideArrow,
+    } = useSlidesContext();
 
-        if (scrollPosition > 0) {
-            setShouldShowArrow(false);
-        } else {
-            setShouldShowArrow(true);
-        }
+    slideRefs.current = edges.map(
+        (_, index) => slideRefs?.current?.[index] || createRef(),
+    );
+
+    const handleScroll = (event: WheelEvent) => {
+        scrollHorizontally(event, contentRef);
+
+        const getCurrentSlide = () => {
+            if (
+                findNextSlide() ===
+                    slideRefs?.current?.[slideRefs?.current?.length - 1] ||
+                nextSlide === slideRefs?.current?.[slideRefs?.current?.length]
+            ) {
+                return slideRefs?.current?.[slideRefs?.current?.length - 2];
+            } else {
+                return slideRefs?.current?.[
+                    slideRefs?.current?.indexOf(findNextSlide()) - 1
+                ];
+            }
+        };
+
+        window &&
+            window.history.pushState(
+                { title: `#${getCurrentSlide()?.current?.id}` },
+                null,
+                `#${getCurrentSlide()?.current?.id}`,
+            );
+
+        setNextSlide(findNextSlide());
     };
 
     const handleScrollToNextSlide = () => {
-        setShouldShowArrow(false);
-        scrollToElement(slideRefs[1]);
+        scrollToElement(nextSlide);
+
+        window &&
+            window.history.pushState(null, null, `#${nextSlide.current.id}`);
+
+        setTimeout(() => {
+            setNextSlide(
+                (prevSlide) =>
+                    slideRefs?.current?.[
+                        slideRefs?.current?.indexOf(prevSlide) + 1
+                    ],
+            );
+        }, 300);
     };
 
-    useMountEffect(() => setSlideRefs(slideRefsArr));
+    useMountEffect(() => {
+        setNextSlide(findNextSlide());
+    });
+
+    useEffect(() => {
+        const listenToHashChange = () => {
+            if (window && window.location.hash.includes('#contact')) {
+                hideArrow();
+            } else {
+                showArrow();
+            }
+        };
+
+        window && window.addEventListener('popstate', listenToHashChange);
+
+        return () => {
+            window &&
+                window.removeEventListener('popstate', listenToHashChange);
+        };
+    }, [hideArrow, showArrow]);
+
+    useEffect(() => {
+        if (
+            findNextSlide() ===
+                slideRefs?.current?.[slideRefs?.current?.length - 1] ||
+            nextSlide === slideRefs?.current?.[slideRefs?.current?.length]
+        ) {
+            hideArrow();
+        } else {
+            showArrow();
+        }
+    }, [nextSlide, slideRefs, findNextSlide, hideArrow, showArrow]);
 
     return (
         <Styled.Wrapper>
-            <Styled.ArrowWrapper
+            <NavArrow
                 shouldShowArrow={shouldShowArrow}
+                isArrowVisible={isArrowVisible}
+                shouldAnimate={nextSlide === slideRefs?.current?.[1]}
                 onClick={handleScrollToNextSlide}
-            >
-                <ArrowRight size={36} />
-            </Styled.ArrowWrapper>
-            <Styled.ScrollWrapper
-                ref={contentRef}
-                onWheel={handleHorizontalScroll}
-            >
-                {edges.map((edge) => {
-                    const slideRef = useRef<HTMLDivElement>();
-                    slideRefsArr.push(slideRef);
-
-                    return (
-                        <Slide
-                            title={edge.node.frontmatter.title}
-                            body={edge.node.body}
-                            key={edge.node.id}
-                            id={edge.node.frontmatter.title}
-                            ref={slideRef}
-                        />
-                    );
-                })}
+            />
+            <Styled.ScrollWrapper ref={contentRef} onWheel={handleScroll}>
+                {edges.map((edge, index) => (
+                    <Slide
+                        title={edge.node.frontmatter.title}
+                        body={edge.node.body}
+                        key={edge.node.id}
+                        id={edge.node.frontmatter.slug}
+                        image={
+                            edge.node.frontmatter.image?.childImageSharp
+                                ?.gatsbyImageData
+                        }
+                        demo={edge.node.frontmatter.demo}
+                        github={edge.node.frontmatter.github}
+                        ref={slideRefs?.current?.[index]}
+                    />
+                ))}
             </Styled.ScrollWrapper>
         </Styled.Wrapper>
     );
