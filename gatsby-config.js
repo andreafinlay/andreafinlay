106a29c5c77028914a0b7505417598ca1976a6f2
andreafinlay <andrea.a.finlay@gmail.com>
2023-05-30 15:38:04 -0400

    fix(deps): add sitemap and robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,21 @@ const path = require('path');
 require('dotenv').config({
     path: `.env.${process.env.NODE_ENV}`,
 });
+const {
+    NODE_ENV,
+    URL: NETLIFY_SITE_URL = 'https://andreafinlay.com',
+    DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+    CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
 
 module.exports = {
     siteMetadata: {
         title: 'andrea finlay | web developer',
         description: 'personal portfolio site for andrea finlay',
         author: 'andrea finlay',
+        siteUrl: siteUrl,
     },
     plugins: [
         'gatsby-plugin-typescript',
@@ -46,7 +55,6 @@ module.exports = {
                             }
                         }
                     },
-                    'gatsby-remark-static-images',
                 ],
             }
         },
@@ -74,5 +82,28 @@ module.exports = {
         },
         'gatsby-plugin-portal',
         'gatsby-plugin-smoothscroll',
+        'gatsby-plugin-sitemap',
+        {
+            resolve: 'gatsby-plugin-robots-txt',
+            options: {
+                sitemap: 'https://andreafinlay.com/sitemap-index.xml',
+                resolveEnv: () => NETLIFY_ENV,
+                env: {
+                        production: {
+                        policy: [{ userAgent: '*' }]
+                    },
+                    'branch-deploy': {
+                        policy: [{ userAgent: '*', disallow: ['/'] }],
+                        sitemap: null,
+                        host: null
+                    },
+                    'deploy-preview': {
+                        policy: [{ userAgent: '*', disallow: ['/'] }],
+                        sitemap: null,
+                        host: null
+                    },
+                },
+            },
+        },
     ],
 };
