5614a696efba802d0cb5845627ccb93ab971609a
andreafinlay <andrea.a.finlay@gmail.com>
2023-05-30 20:33:31 -0400

    feat(theme): create theme toggle button

---
+++ src/components/toggleButton/toggleButton.styled.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Button = styled('button')`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 4px;
+    border-radius: 25%;
+    background-color: ${({ theme }) => theme.text};
+    color: ${({ theme }) => theme.body};
+    border-color: ${({ theme }) => theme.body};
+    margin-left: 24px;
+
+    &:hover {
+        background-color: ${({ theme }) => theme.text};
+        color: ${({ theme }) => theme.body};
+    }
+`;
+
+export const Styled = {
+    Button,
+};
